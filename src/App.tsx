b5b4415f6701a41e500c989d6f078e10983019b3
Nitinkumar Thawkar <nitin.thawkar@gmail.com>
2025-07-24 14:58:11 +0530

    MuiTypography working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import  { MuiTypography } from './Mui_components/MuiTypography'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
       {/* <div>
@@ -28,6 +25,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
+
+      <MuiTypography />
     </>
   )
 }
---
+++ src/Mui_components/MuiTypography.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+export const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant="h1">Heading 1</Typography>
+        <Typography variant="h2">Heading 2</Typography>
+        <Typography variant="body1">Body text</Typography>
+    </div>
+  )
+}
